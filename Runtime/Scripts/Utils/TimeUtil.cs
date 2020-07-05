01cf666bede7f1164690610c2910fc24a7722546
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2020-07-05 18:10:29 -0400

    updating to the latest version of Unity and the Entities package - and adding a time offset property to the time utils class

--- Runtime/Scripts/Utils/TimeUtil.cs
+++ Runtime/Scripts/Utils/TimeUtil.cs
@@ -3,9 +3,10 @@ using UnityEngine;
 namespace Software10101.DOTS.Utils {
     public static class TimeUtil {
         public static float FixedDeltaTime => Time.fixedDeltaTime;
-        public static float NextFixedTime => Time.fixedTime;
-        public static float PreviousFixedTime => NextFixedTime - FixedDeltaTime;
-        public static float PresentationTime => Time.time - FixedDeltaTime;
+        public static float NextFixedTime => Time.fixedTime + TimeOffset;
+        public static float PreviousFixedTime => NextFixedTime - FixedDeltaTime + TimeOffset;
+        public static float PresentationTime => Time.time - FixedDeltaTime + TimeOffset;
         public static float PresentationTimeFraction => (PresentationTime - PreviousFixedTime) / (NextFixedTime - PreviousFixedTime);
+        public static float TimeOffset = 0.0f;
     }
 }
