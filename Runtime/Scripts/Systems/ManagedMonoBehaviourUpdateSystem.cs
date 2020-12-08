3224752495a2e027c0b6fe8c8ba65f4484970a51
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2020-12-08 18:14:53 -0500

    completeing system jobs before calling Update on managed MonoBehaviours

--- Runtime/Scripts/Systems/ManagedMonoBehaviourUpdateSystem.cs
+++ Runtime/Scripts/Systems/ManagedMonoBehaviourUpdateSystem.cs
@@ -4,6 +4,7 @@ using Unity.Entities;
 namespace Software10101.DOTS.Systems {
     internal sealed class ManagedMonoBehaviourUpdateSystem : SystemBase {
         protected override void OnUpdate() {
+            Dependency.Complete();
             ManagedMonoBehaviour.DoUpdate();
         }
     }
