dca24a872727249f835ecedec74926b0c94002d9
Tristan Bellman-Greenwood <tbg10101@gmail.com>
2022-06-16 00:24:08 -0400

    updating burst settings, adding a value sharing example

---
+++ Examples~/Hybrid Simulation Test Project/Assets/Scripts/Systems/SharedValueSystemReference.cs
@@ -0,0 +1,33 @@
+using Software10101.DOTS.Example.Data;
+using Software10101.DOTS.MonoBehaviours;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Software10101.DOTS.Example.Systems {
+    [CreateAssetMenu(menuName = "Systems/" + nameof(SharedValueSystem))]
+    public class SharedValueSystemReference : SystemTypeReference<SharedValueSystem> { }
+
+    // ReSharper disable once PartialTypeWithSinglePart // systems need to be partial after Entities 0.50
+    public partial class SharedValueSystem : SystemBase {
+        protected override void OnUpdate() {
+            NativeReference<int> nativeReference = new NativeReference<int>(0, Allocator.TempJob);
+
+            // do something to populate the value
+            Entities
+                .ForEach((in PositionComponentData component) => {
+                    nativeReference.Value++;
+                })
+                .Schedule();
+
+            // read the value in another job - the dependency is already managed by the SystemBase' implicit JobHandle
+            Job
+                .WithReadOnly(nativeReference)
+                .WithCode(() => {
+                    Debug.Log($"Position Count: {nativeReference.Value}");
+                })
+                .WithDisposeOnCompletion(nativeReference)
+                .Schedule();
+        }
+    }
+}
